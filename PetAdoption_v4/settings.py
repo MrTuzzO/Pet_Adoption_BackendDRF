4276e31be2e90489595be68c8442805fc2a4568f
MrTuzzO <khirulislam5750@gmail.com>
2025-02-02 23:49:17 +0600

    cross headaer added

--- PetAdoption_v4/settings.py
+++ PetAdoption_v4/settings.py
@@ -32,26 +32,19 @@ ALLOWED_HOSTS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CSRF_TRUSTED_ORIGINS = [
-    # 'http://localhost:3000',
-    # 'http://127.0.0.1:5500',
-    # 'https://mrtuzzo.github.io/',
-    # 'http://127.0.0.1',
-    # 'https://pet-adoption-drf.onrender.com',
+    'http://127.0.0.1',
+    'https://mrtuzzo.github.io/',
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    # 'http://127.0.0.1:8000',
-    # 'http://localhost:8000',
-    # 'http://localhost:3000',
-    # 'http://127.0.0.1:5500',
-    # 'http://127.0.0.1',
-    # 'https://mrtuzzo.github.io',
+    'http://127.0.0.1',
+    'https://mrtuzzo.github.io',
 )
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
-    # "http://localhost:5500",
-    # 'https://mrtuzzo.github.io',
+    "http://localhost:5500",
+    'https://mrtuzzo.github.io',
 ]
 
 # Application definition
